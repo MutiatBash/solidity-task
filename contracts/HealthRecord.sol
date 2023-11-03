515a57b973562f664d8cc8fb979db3d46515b925
MutiatBash <mutiatbashua@gmail.com>
2023-11-03 14:52:18 +0100

    updated code

--- contracts/HealthRecord.sol
+++ contracts/HealthRecord.sol
@@ -5,26 +5,29 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract HealthRecords {
 
-    uint256 public age;
-    string public gender;
-    string public patientName;
+    struct Patients { 
+    uint256 age;
+    PatientsGender gender;
+    string patientName;
     uint256 patientId;
     string admissionDate;
-    uint256 internal dischargeDate;
-    string private condition;
+    string condition;
+    bool discharged;
+    }
 
+    enum PatientsGender{
+       Female,
+       Male
+    }
+
+    Patients [] public patients;
     
-    function store(uint256 _age,string memory name,string memory _gender, uint256 _patientId, string memory _admissionDate, string memory _condition) public {
-        age = _age;
-        patientName = name;
-        gender = _gender;
-        patientId = _patientId;
-        admissionDate = _admissionDate;
-        condition = _condition;
+    function registerPatients(uint256 _age, string calldata _patientName, PatientsGender _gender, uint256 _patientId, string calldata _admissionDate, string calldata _condition, bool _discharged) public {
+        patients.push(Patients(_age, _gender,_patientName, _patientId, _admissionDate, _condition, _discharged));
     }
 
 
-    function retrieve() public view returns (uint256, string memory, string memory,uint256, string memory){
-        return (age,patientName,gender,patientId,admissionDate);
-    }
+    // function retrievePatients() public view returns (uint256, string memory, PatientsGender,uint256, string memory){
+    //     return ();
+    // }
 }
